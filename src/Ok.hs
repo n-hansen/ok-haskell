a24926f0056c225cf08adefc6798b0169791244b
nick hansen <n-hansen@users.noreply.github.com>
2019-06-10 00:50:43 -0700

    parser works

--- src/Ok.hs
+++ src/Ok.hs
@@ -5,10 +5,19 @@
   --package turtle
 -}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies      #-}
 module Ok where
 
+import           Control.Monad
+import           Data.Char
+import           Data.Maybe
 import           Data.Text
-import           Turtle
+import           Data.Void
+import           Text.Megaparsec
+import qualified Text.Megaparsec      as MP
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char as MP
+import           Turtle               hiding (Parser, sepBy)
 
 main :: IO ()
 main = echo "Hello World!"
@@ -17,9 +26,35 @@ main = echo "Hello World!"
 --- Types ---
 
 data Command = Command { commandString :: Text
+                       , docString     :: Maybe Text
+                       , alias         :: Maybe Text
                        } deriving (Eq,Show)
 
 --- File Parsing ---
 
+type Parser = Parsec Void Text
+
+cmdParser :: Parser (Maybe Command)
+cmdParser = do
+  a <- aliasParser
+  cs <- csParser
+  ds <- dsParser
+  if cs == mempty
+    then return Nothing
+    else return . Just $ Command cs ds a
+
+  where
+    csParser :: Parser Text
+    csParser = strip <$> takeWhileP (Just "command string") (and <$> sequence [(/= '#'), (/= '\n')])
+
+    dsParser :: Parser (Maybe Text)
+    dsParser = optional $ fmap strip $ takeWhile1P Nothing (== '#') *> takeWhileP (Just "doc string") (/= '\n')
+
+    aliasParser :: Parser (Maybe Text)
+    aliasParser = optional $ try $ takeWhile1P (Just "alias") isAlphaNum <* MP.char ':' <* space1
+
 parseOkText :: Text -> [Command]
-parseOkText txt = [Command txt]
+parseOkText = join . maybeToList . parseMaybe multilineParser
+  where
+    multilineParser :: Parser [Command]
+    multilineParser = catMaybes <$> cmdParser `sepBy` MP.newline
--- tests/test.hs
+++ tests/test.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
+import qualified Data.Text as T
+import           Ok               hiding (main)
 import           Test.Tasty
 import           Test.Tasty.HUnit
-
-import           Ok               hiding (main)
 
 main :: IO ()
 main = defaultMain tests
@@ -13,7 +13,37 @@ tests =
   [ testGroup "unit tests"
     [ testGroup ".ok file parser"
       [ parserTestCase "parser test 1"
-        "foo bar" $ [Command "foo bar"]
+        "foo bar"
+        [Command "foo bar" Nothing Nothing]
+      , parserTestCase "parser test 2"
+        "foo bar # apply foo to bar"
+        [Command "foo bar" (Just "apply foo to bar") Nothing]
+      , parserTestCase "parser test 3"
+        ( T.unlines [ "foo bar # apply foo to bar"
+                    , "baz"
+                    , "quux -v # verbose quux"
+                    ]
+        )
+        [ Command "foo bar" (Just "apply foo to bar") Nothing
+        , Command "baz" Nothing Nothing
+        , Command "quux -v" (Just "verbose quux") Nothing
+        ]
+      , parserTestCase "parser test 4"
+        ( T.unlines [ "# comment"
+                    , "foo bar"
+                    ]
+        )
+        [Command "foo bar" Nothing Nothing]
+      , parserTestCase "parser test 5"
+        ( T.unlines [ "foo bar # apply foo to bar"
+                    , "flub: baz"
+                    , "bort: quux -v # verbose quux"
+                    ]
+        )
+        [ Command "foo bar" (Just "apply foo to bar") Nothing
+        , Command "baz" Nothing (Just "flub")
+        , Command "quux -v" (Just "verbose quux") (Just "bort")
+        ]
       ]
     ]
   ]
