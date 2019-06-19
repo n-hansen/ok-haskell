93ea196903d34d0c246e98a90bc16434ca5a741c
nick hansen <n-hansen@users.noreply.github.com>
2019-06-18 23:40:29 -0700

    fancier fish thing

--- ok.fish
+++ ok.fish
@@ -2,8 +2,12 @@ set -g ok_path "~/bin/Ok.hs"
 
 function ok
     if test (count $argv) -gt 0
-        eval (eval "$ok_path" $argv)
+        set -l ok_dir (eval $ok_path -sl)
+        set -l ok_cmd (eval $ok_path -s $argv)
+        pushd $ok_dir
+        eval $ok_cmd
+        popd
     else
-        eval "$ok_path"
+        eval $ok_path -s
     end
 end
